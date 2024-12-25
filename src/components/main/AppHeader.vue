75f4efa824ab1d2bb6a48eafcf4ba0f2d48bf27b
Dima Polo <prolonchuk5@gmail.com>
2024-12-25 20:40:03 +0200

    App header

--- src/components/main/AppHeader.vue
+++ src/components/main/AppHeader.vue
@@ -1,6 +1,17 @@
 <template>
     <header class="app-header">
-        <h1>Home</h1>
+        <router-link class="main-logo" to="/">
+            <img height="42"
+                src="https://cdn.prod.website-files.com/61e95e54543a7c0a5967ffd1/635a79ad72a1555286e378b9_RunAi_Logo_Digital_2C_BP.svg"
+                alt="Main Logo" />
+        </router-link>
+        <template v-if="companies">
+            <router-link class="header-link" v-for="company in companies" :key="company._id"
+                :to="'/company/' + company._id">
+                {{ company.name }}
+            </router-link>
+        </template>
+
     </header>
 </template>
 
@@ -9,8 +20,11 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'App Header',
-
+    computed: {
+        companies() {
+            console.log("🚀 ~ companies ~ this.$store.getters.companies;:", this.$store.getters.companies)
+            return this.$store.getters.companies;
+        }
+    }
 });
 </script>
-
-<style scoped></style>
