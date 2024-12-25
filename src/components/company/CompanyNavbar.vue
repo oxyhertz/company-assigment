0a0910a60fcb19b05b85eb21bddc70c932e0e3a8
Dima Polo <prolonchuk5@gmail.com>
2024-12-25 23:52:01 +0200

    handle logics

--- src/components/company/CompanyNavbar.vue
+++ src/components/company/CompanyNavbar.vue
@@ -1,10 +1,28 @@
 <template>
     <nav class="company-navbar">
-
+        <router-link :to="`/company/${currCompanyId}`">Departments</router-link>
+        <router-link :to="`/company/${currCompanyId}/employee`">Employees</router-link>
     </nav>
 </template>
-<script>
-export default {
-    name: 'Company Navbar'
-}
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+    name: 'Company Navbar',
+    props: {
+        currCompanyId: {
+            type: String,
+            required: true
+        }
+    },
+    created() {
+        this.currCompanyId
+        console.log("🚀 ~ created ~   this.currCompanyId:", this.currCompanyId)
+
+    },
+
+});
 </script>
+
+<style scoped></style>
--- src/views/CompanyIndex.vue
+++ src/views/CompanyIndex.vue
@@ -1,24 +1,52 @@
 <template>
     <section class="company-index">
-        <CompanyNavbarVue />
+        <CompanyNavbarVue :currCompanyId="currCompanyId" />
         <div v-if="company" class="company-details-container">
             <div class="company-details">
                 <h1>{{ company?.name }}</h1>
             </div>
-            <router-view @add-department="onAddDepartment" @remove-department="onRemoveDepartment" />
+            <router-view @add-employee="onAddEmployee" @remove-employee="onRemoveEmployee"
+                @add-department="onAddDepartment" @remove-department="openRemoveDepartmentModal" />
         </div>
         <div v-else>
             <h2>Loading...</h2>
         </div>
-        <Modal :isOpen="isRemoveDepartmentModalOpen" @close="isRemoveDepartmentModalOpen = false">
+
+
+
+        <Modal :isOpen="isRemoveDepartmentModalOpen" @close="closeRemoveDepartmentModalOpen">
             <div class="modal-content">
                 <h2>Are you sure you want to remove this department?</h2>
                 <div class="modal-buttons">
-                    <button @click="isRemoveDepartmentModalOpen = false">Cancel</button>
-                    <button @click="onRemoveDepartment(departmentId)">Remove</button>
+                    <button @click="closeRemoveDepartmentModalOpen">Cancel</button>
+                    <button @click="onRemoveDepartment()">Remove</button>
                 </div>
             </div>
         </Modal>
+
+
+        <Modal :isOpen="isDepartmentEmployeeModalOpen" @close="isDepartmentEmployeeModalOpen = false">
+            <div class="modal-content">
+                <h2>This Department have employees</h2>
+                <h2>Do you want to move them to another department?</h2>
+                <div class="modal-buttons" v-if="!isSelcetDepartments">
+                    <button @click="handleDontMoveToDepartment">No</button>
+                    <button @click="isSelcetDepartments = true">Yes</button>
+                </div>
+                <div v-if="isSelcetDepartments" class="select-department">
+                    <select v-model="moveEmployeesToDepartmentId">
+                        <option value="">Select Department</option>
+                        <option v-for="department in departmentsOptionsToDisplay" :key="department._id"
+                            :value="department._id">
+                            {{ department.name }}
+                        </option>
+                    </select>
+                    <button @click="onMoveEmployeesToDepartment">Move</button>
+                </div>
+            </div>
+        </Modal>
+
+
     </section>
 </template>
 
@@ -26,6 +54,8 @@
 
 import { defineComponent } from 'vue';
 import type { Company } from '@/types/company/company';
+import type { Department } from '@/types/company/department';
+import type { Employee } from '@/types/company/employee';
 import CompanyNavbarVue from '@/components/company/CompanyNavbar.vue';
 import { showSuccessMsg, showErrorMsg } from '@/services/event-bus.service';
 import { DepartmentToSave } from '@/types/company/department';
@@ -35,7 +65,11 @@ export default defineComponent({
     data() {
         return {
             isRemoveDepartmentModalOpen: false,
+            isDepartmentEmployeeModalOpen: false,
             departmentId: null as null | string,
+            moveEmployeesToDepartmentId: null as null | string,
+            isSelcetDepartments: null as null | boolean,
+            isMoveToDepartment: null as null | boolean
         }
     },
     methods: {
@@ -48,15 +82,28 @@ export default defineComponent({
         },
         openRemoveDepartmentModal(departmentId: string) {
             this.departmentId = departmentId;
-            this.isRemoveDepartmentModalOpen = true;
+            if (this.isDepartmentHaveEmployees) {
+                this.isDepartmentEmployeeModalOpen = true;
+            } else {
+                this.openIsRemoveDepartmentModal();
+            }
         },
         async onRemoveDepartment() {
             try {
-                await this.$store.dispatch({ type: 'removeDepartment', departmentId: this.departmentId });
+                await this.$store.dispatch({ type: 'removeDepartment', departmentId: this.departmentId, moveToDepartmentId: this.isMoveToDepartment ? this.moveEmployeesToDepartmentId : null });
+                this.resetState();
                 showSuccessMsg('Department removed successfully');
             } catch (error) {
                 showErrorMsg('Failed to remove department');
             }
+        },
+        resetState() {
+            this.isRemoveDepartmentModalOpen = false;
+            this.isDepartmentEmployeeModalOpen = false;
+            this.departmentId = null;
+            this.moveEmployeesToDepartmentId = null;
+            this.isSelcetDepartments = null;
+            this.isMoveToDepartment = null;
         },
         async onAddDepartment(department: DepartmentToSave) {
             try {
@@ -66,6 +113,41 @@ export default defineComponent({
                 showErrorMsg('Failed to add department');
             }
         },
+        async onAddEmployee(employee: Employee) {
+            try {
+                await this.$store.dispatch({ type: 'addEmployee', employee });
+                showSuccessMsg('Employee added successfully');
+            } catch (error) {
+                showErrorMsg('Failed to add employee');
+            }
+        },
+        async onRemoveEmployee(employeeId: string) {
+            try {
+                await this.$store.dispatch({ type: 'removeEmployee', employeeId });
+                showSuccessMsg('Employee removed successfully');
+            } catch (error) {
+                showErrorMsg('Failed to remove employee');
+            }
+        },
+        onMoveEmployeesToDepartment() {
+            this.isMoveToDepartment = true
+            this.isDepartmentEmployeeModalOpen = false
+
+            this.openIsRemoveDepartmentModal()
+
+        },
+        openIsRemoveDepartmentModal() {
+            this.isRemoveDepartmentModalOpen = true
+        },
+        handleDontMoveToDepartment() {
+            this.isDepartmentEmployeeModalOpen = false
+
+            this.openIsRemoveDepartmentModal()
+        },
+        closeRemoveDepartmentModalOpen() {
+            this.isRemoveDepartmentModalOpen = false
+        }
+
     },
 
     computed: {
@@ -74,7 +156,18 @@ export default defineComponent({
         },
         company(): Company | null {
             return this.$store.getters.currentCompany;
+        },
+        isDepartmentHaveEmployees(): boolean {
+            if (this.company) {
+                return this.company.employees.some((emp: Employee) => emp.depId === this.departmentId);
+            }
+            return false;
+        },
+        departmentsOptionsToDisplay() {
+            return this.company?.departments.filter((department: Department) => department._id !== this.departmentId)
         }
+
+
     },
     watch: {
         currCompanyId: {
